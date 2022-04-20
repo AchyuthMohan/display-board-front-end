758512c599ce5d7ca8f7b7e2923d1b3c4486c35e
Achyuth Mohan <achyuthmohan1@gmail.com>
2022-04-20 18:04:53 +0530

    api integrated

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -1,25 +1,23 @@
 import React from 'react'
 import './Card.css'
-const Card = () => {
+// import Home from '../../Pages/Home/Home'
+const Card = (props) => {
   return (
     <div class="container">
     <div class="card">
-      <img src="https://lh3.googleusercontent.com/ytP9VP86DItizVX2YNA-xTYzV09IS7rh4WexVp7eilIcfHmm74B7odbcwD5DTXmL0PF42i2wnRKSFPBHlmSjCblWHDCD2oD1oaM1CGFcSd48VBKJfsCi4bS170PKxGwji8CPmehwPw=w200-h247-no" alt="Person" class="card__image"/>
-      <p class="card__name">Lily-Grace Colley</p>
+      <img src={props.img} alt="Person" class="card__image"/>
+      <p class="card__name">{props.name}</p>
       <div class="grid-container">
   
-        <div class="grid-child-posts">
+        {/* <div class="grid-child-posts">
           156 Post
-        </div>
+        </div> */}
   
-        <div class="grid-child-followers">
-          1012 Likes
-        </div>
-  
+        
       </div>
      
-      <button class="btn draw-border">Follow</button>
-      <button class="btn draw-border">Message</button>
+      <button class="btn draw-border">Price: {props.price}</button>
+      {/* <button class="btn draw-border">Message</button> */}
   
     </div>
     
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,22 +1,42 @@
-import React, { Component } from 'react'
-import Card from '../../Components/Card/Card'
-import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+import Card from '../../Components/Card/Card';
 
-const api=axios.create({
-  baseURL:`http://127.0.0.1:8000/products/`
-})
-export class Home extends Component {
-  constructor(){
-    super();
-    api.get('/').then(res=>{
-      console.log(res.data.id)
-    })
-  }
-  render() {
+function Home() {
+  const[error,setError]=useState(null);
+  const[isLoaded,setIsLoaded]=useState(false);
+  const[items,setItems]=useState([]);
+
+  useEffect(() => {
+    fetch("http://127.0.0.1:8000/products/")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          setIsLoaded(true);
+          setItems(result);
+        },
+        (error) => {
+          setIsLoaded(true);
+          setError(error);
+        }
+      )
+  }, [])
+  
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  } else if (!isLoaded) {
+    return <div>Loading...</div>;
+  } else {
     return (
-      <div>Home</div>
-    )
+      <ul>
+        {items.map(item => (
+          <div key={item.id}>
+            <Card name={item.name}image={item.img}price={item.price}/>
+          </div>
+          
+        ))}
+      </ul>
+    );
   }
 }
-
 export default Home
